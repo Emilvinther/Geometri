9780471ee2f63defb5827d17cd415d7782376408
Emil Lorentzen <emilerher@gmail.com>
2022-03-28 11:17:46 +0200

    Add project files.

---
+++ Geometri/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Geometri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(4.5);
+            Console.WriteLine($"Arealet er {square.Area()} cm");
+            Console.WriteLine($"Omkredsen er {square.Perimitor()} cm");
+
+            Square squarea = new Square(12);
+            Console.WriteLine($"\nArealet er {squarea.Area()} cm");
+            Console.WriteLine($"Omkredsen er {squarea.Perimitor()} cm");
+
+            Square squareb = new Square(10);
+            Console.WriteLine($"\nArealet er {squareb.Area()} cm");
+            Console.WriteLine($"Omkredsen er {squareb.Perimitor()} cm");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Geometri/Square.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    public class Square
+    {
+        private double sidea;
+
+        public double Sidea { get => sidea; set => sidea = value; }
+
+        public Square()
+        {
+
+        }
+
+        public Square(double sidea)
+        {
+            Sidea = sidea;
+        }
+        
+        public double Perimitor()
+        {
+            return sidea + sidea + sidea + sidea;
+        }
+
+        public double Area()
+        {
+            return sidea * sidea;
+        }
+    }
+}
